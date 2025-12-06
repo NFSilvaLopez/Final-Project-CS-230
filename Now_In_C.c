3702e7992c69ca7fa2fd542afd05cac8900574af
Noah Silva-Lopez <117937042+NFSilvaLopez@users.noreply.github.com>
2025-12-05 21:14:51 -0500

    Fixed the Code
    Can now read X as a exit, and continues to loop until exit.

--- Now_In_C.c
+++ Now_In_C.c
@@ -3,12 +3,28 @@
 void translate(int num);
 
 int main(){
+char input;
 int num;
 
-printf("Please enter a hex digit: \n");
-scanf("%x", &num);
-translate(num);
-
+while (1){
+     printf("Please enter a hex digit or X to exit: \n");
+    scanf(" %c", &input);
+    if(input == 'X' || input == 'x'){
+        break;
+    } 
+    if (input >= '0' && input <= '9'){
+        num = input - '0';
+    } 
+    else if (input >= 'a' && input <= 'f'){
+        num = input - 'a' + 10;
+        printf("%d", num);
+        printf("\n");
+}  
+else if (input >= 'A' && input <= 'F'){
+        num = input - 'A' + 10;
+        }
+        translate(num);
+    }
 return 0;
 }
 
@@ -16,4 +32,5 @@ void translate(int num){
     for(int i = 3; i >= 0; i--){
         printf("%d", (num >> i) & 1);
     }
+    printf("\n");
 }
